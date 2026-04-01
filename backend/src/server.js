7cd6f2909e191bf58abe87d9b0e8ab7b741077f4
Erik Jashari <erikjashari3@gmail.com>
2026-04-01 15:29:05 +0200

    Connected with DB and fixed files structure.And run npm i.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,26 @@
 import express from 'express'
+import pool from './config/db.js';
 const app = express();
 const port = 3005;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
-    
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
+async function testConnection() {
+  try {
+    const client = await pool.connect();
+    console.log('U lidh me databazen me sukses!');
+    client.release();
+    process.exit(0);
+  } catch (err) {
+    console.error('Lidhja deshtoi:', err.message);
+    process.exit(1);
+  }
+}
+
+testConnection();
